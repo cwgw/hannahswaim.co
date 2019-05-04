485a258ee14d48f2fdc7bc423136388860377341
Charlie Wright <charlie@cwgw.co>
2019-05-04 12:50:26 -0700

    Ensure scrolling is unaffected on mobile (no modal)

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,12 +1,14 @@
 import _get from 'lodash/get'
 
+import { breakpoints, modalBreakpoint } from 'style/constants'
 import WrapPage from './wrap-page'
 
 export const wrapPageElement = WrapPage
 
 export const shouldUpdateScroll = ({routerProps}) => {
-  const {origin, enableModal} = _get(routerProps, 'location.state') || {}
-  return !(enableModal || origin === 'modal')
+  const { origin, enableModal } = _get(routerProps, 'location.state') || {}
+  const canHaveModal = typeof window !== 'undefined' && window.innerWidth >= breakpoints.get(modalBreakpoint);
+  return !((enableModal || origin === 'modal') && canHaveModal)
 }
 
 export const onInitialClientRender = () => {
